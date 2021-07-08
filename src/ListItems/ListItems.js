ccdedc5067f48affa7396b47bc0f654a0af1f565
unknown <hmanaog@gmail.com>
2021-07-08 18:09:27 +0200

    Added sorting function

--- src/ListItems/ListItems.js
+++ src/ListItems/ListItems.js
@@ -1,12 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import { withRouter, Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
-import { Box, Grid, Card, CardContent, CardMedia, Typography } from '@material-ui/core';
+import { Box, Button, NativeSelect, Grid, Card, CardContent, CardMedia, Typography } from '@material-ui/core';
 
 // Styles
 const useStyles = makeStyles({
    box: {
       margin: '0 0',
+   },
+   wrapper: {
+      display: 'flex',
+      justifyContent: 'space-between',
+      flexDirection: 'row',
+      alignItems: 'center',
    },
    grid: {
       marginTop: '1rem',
@@ -51,10 +57,26 @@ const ListItems = (props) => {
       totalCount: 0,
    });
 
+   const [sortedItems, setSortedItems] = useState([]);
+   const [value, setValue] = useState();
+
    // Initiate useEffect when component mount
    useEffect(() => {
       GetItemsFromRicardo();
    }, []);
+
+   useEffect(() => {
+      switch (value) {
+         case 'lower':
+            return setSortedItems([...items.articles].sort(compareLowest));
+         case 'higher':
+            return setSortedItems([...items.articles].sort(compareHighest));
+         case 'ending':
+            return setSortedItems([...items.articles].sort(compareEnddate));
+         default:
+            return setSortedItems(items.articles);
+      }
+   }, [value, items]);
 
    // Fetch list of items from Ricardo API
    const GetItemsFromRicardo = async () => {
@@ -75,15 +97,70 @@ const ListItems = (props) => {
          });
    };
 
+   // Function to sort by lowest price
+   const compareLowest = (a, b) => {
+      if (a.buyNowPrice < b.buyNowPrice) {
+         return -1;
+      }
+      if (a.buyNowPrice > b.buyNowPrice) {
+         return 1;
+      }
+      return 0;
+   };
+
+   // Function to sort by highest price
+   const compareHighest = (a, b) => {
+      if (a.buyNowPrice > b.buyNowPrice) {
+         return -1;
+      }
+      if (a.buyNowPrice < b.buyNowPrice) {
+         return 1;
+      }
+      return 0;
+   };
+
+   // Function to sort by ending date
+   const compareEnddate = (a, b) => {
+      if (a.endDate < b.endDate) {
+         return -1;
+      }
+      if (a.endDate > b.endDate) {
+         return 1;
+      }
+      return 0;
+   };
+
+   const handleSelectChange = (event) => {
+      setValue(event.target.value);
+   };
+
    return (
       <Box className={classes.box}>
-         <Typography variant='body2' color='textPrimary' component='p'>
-            Total count: {items.articles.length}{' '}
-         </Typography>
+         <Box className={classes.wrapper}>
+            <Typography variant='body2' color='textPrimary' component='p'>
+               Total count: {items.articles.length}{' '}
+            </Typography>
+            <Box className={classes.wrapper}>
+               <Typography
+                  variant='body2'
+                  color='textPrimary'
+                  component='p'
+                  style={{ fontSize: '18px', marginRight: '0.2rem' }}
+               >
+                  Sort by:
+               </Typography>{' '}
+               <NativeSelect value={value} style={{ fontSize: '18px' }} onChange={handleSelectChange} name='itemSort'>
+                  <option value={'default'}>Relevance</option>
+                  <option value={'lower'}>Lowest price</option>
+                  <option value={'higher'}>Highest price</option>
+                  <option value={'ending'}>Ending soon</option>
+               </NativeSelect>
+            </Box>
+         </Box>
 
          <Grid className={classes.grid} container spacing={3}>
-            {items &&
-               items.articles.map((item, index) => {
+            {sortedItems &&
+               sortedItems.map((item, index) => {
                   return (
                      <Grid item xs={12} sm={3} key={index}>
                         <Link className={classes.link} to={`/article/${item.id}`}>
